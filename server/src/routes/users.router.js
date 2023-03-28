4f2658bb1882e6480d69dabd6ff828fd7a47cda8
Matias Paredes <tutecapo1985@gmail.com>
2023-03-28 20:57:47 -0300

    Adding dependency on template engines, performing setup.

--- server/src/routes/users.router.js
+++ server/src/routes/users.router.js
@@ -1,6 +1,32 @@
 import { Router } from "express";
-import { usersModel} from '../models/users.model.js'
+import { usersModel } from '../models/users.model.js'
 
 const router = Router()
 
+router.post('/register', async (req, res) => {
+    const { email, password } = req.body
+    try {
+        const userExists = await usersModel.find({ email })
+        if (userExists.length !== 0) {
+            res.redirect('/views/failRegister')
+        } else {
+            res.redirect('/views/login')
+        }
+    } catch (error) {
+        console.log(error)
+
+    }
+
+})
+
+router.post('/login', (req, res) => {
+    res.json({ message: "This is user login" })
+})
+
+router.get('/profile', (req, res) => {
+    res.json({ message: "This is user profile" })
+})
+
 export default router
+
+
