222f17bc026ca42214b6a2f0e72b1aef351d0a9c
naty <natymayorn@gmail.com>
2023-03-29 10:19:37 -0500

    creación de route de prueba, función seteadora(envía datos a mongo) y creación de servicio del flujo de recycle

--- server/src/models/recycles.model.js
+++ server/src/models/recycles.model.js
@@ -3,21 +3,33 @@ import { Schema, model } from "mongoose";
 const recyclesSchema = new Schema({
     currentPoints: {
         type: Number,
-        required: true,
+       // required: true,
+
     },
     materialType: {
         type: String,
-        required: true
+       // required: true
     },
     currentWeight:{
         type: Number,
-        required: true
+       // required: true
     },
-    userID:{
+   /* userID:{
         type: Schema.Types.ObjectId,
         ref: 'Users',
         required: true
-    }
+    }*/
 })
 
+recyclesSchema.path("currentPoints").set(function(currentPoints){
+    //currentPoints = this.currentPoints * this.currentWeight; 
+    console.log(currentPoints, this.currentPoints, this.currentWeight)
+    return currentPoints;
+    
+    //con esta funcion seteadora nos evitamos tener que hacer ese cálculo en cada request en los endpoints  
+    //en el esquema  debo acceder a los datos del request 
+});
+
 export const recyclesModel = model('Recycles', recyclesSchema)
+
+
--- server/src/routes/recycles.router.js
+++ server/src/routes/recycles.router.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import { recyclesModel} from '../models/recycles.model.js'
+import RecyclesManager from "../services/recyclesManager.js";
 
-const router = Router()
+const router = Router();
+const recycles_manager = new RecyclesManager();
+
+router.get("/puntos", (req, res)=>{ 
+    return res.json({message:"hola"});
+})
+
+router.post("/puntos",(req, res) => recycles_manager.createRecycle(req.body)); 
 
 
 
-
-
-export default router
+export default router;
