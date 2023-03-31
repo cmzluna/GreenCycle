bdd3960976d4c35af9ffc47aadea4b88642ea1da
cmzluna <claudiomazzoli@gmail.com>
2023-03-31 16:01:13 -0300

    images and styles

--- client/src/screens/Welcome/styles.js
+++ client/src/screens/Welcome/styles.js
@@ -2,15 +2,55 @@ import styled from 'styled-components/native';
 
 const Container = styled.View`
   display: flex;
-  background-color: white;
   flex: 1;
+  background-color: white;
   justify-content: center;
-  padding: 10px;
+  align-items: flex-end;
+  padding: 30px 15px;
 `;
 
-const Title = styled.Text`
+const InlineContainer = styled.View`
+  display: flex;
+  background-color: white;
+  justify-content: center;
+  align-items: flex-end;
+  margin-bottom: 28px;
+`;
+
+const SlidesContainer = styled.View`
+  display: flex;
+  flex: 1;
+  padding-bottom: 35px;
+  width: 90%;
+  background-color: white;
+  align-self: center;
+`;
+
+const SlideView = styled.View`
+  display: flex;
+  flex: 1;
+  align-items: center;
+`;
+
+const SlideText = styled.Text`
   color: #000;
-  font-size: 32px;
+  font-size: 17px;
 `;
 
-export {Container, Title};
+const SlideTitle = styled.Text`
+  color: #000;
+  font-size: 22px;
+  font-weight: medium;
+  margin-bottom: 20px;
+  text-align: center;
+  margin: 20px 0 17px 0;
+`;
+
+export {
+  Container,
+  InlineContainer,
+  SlideTitle,
+  SlidesContainer,
+  SlideView,
+  SlideText,
+};
